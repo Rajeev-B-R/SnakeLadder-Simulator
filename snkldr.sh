e903e7cba94d460af6f84bf8e9762dd6a9788085
Rajeev <rajeevsuchiha@gmail.com>
2020-08-21 10:56:14 +0530

    Added: Code to check for option added

--- snkldr.sh
+++ snkldr.sh
@@ -1,9 +1,26 @@
 #!/bin/bash -x
 
 pos=0
-dice()
-{
+
+option(){
+	choice=$((RANDOM % 3))
+	echo $choice
+	if [ $choice -eq 2 ]
+	then
+		echo "Ladder"
+		pos=$(($pos + $roll))
+	elif [ $choice -eq 1 ]
+	then
+		echo "Snake"
+		pos=$(($pos - $roll))
+	else
+		echo "No play"
+	fi
+}
+
+dice(){
 	roll=$((RANDOM % 6 + 1))
 	echo "You got $roll"
+	option
 }
 dice
