f7f0b00133b55e01ca845bffe67160e2b46fc350
Rajeev <rajeevsuchiha@gmail.com>
2020-08-21 11:40:20 +0530

    Added: Code added for 2 players

--- snkldr.sh
+++ snkldr.sh
@@ -1,15 +1,19 @@
-#!/bin/bash -x
+#! /bin/bash
 
 pos=0
-declare -A game
-rollCount=1
+player1Pos=0
+player2Pos=0
 
-option(){
+dice(){
+	roll=$((RANDOM % 6 + 1))
+        echo "You got: $roll"
 	choice=$((RANDOM % 3))
 	if [ $choice -eq 2 ]
 	then
 		echo "Ladder"
 		pos=$(($pos + $roll))
+		Condition
+		dice
 	elif [ $choice -eq 1 ]
 	then
 		echo "Snake"
@@ -19,32 +23,42 @@ option(){
 	fi
 }
 
-dice(){
-  roll=$((RANDOM % 6 + 1))
-  echo "You got : $roll"
-  option
-}
-
-while [ $pos -le 100 ]
-do
-	echo $rollCount
-	dice
+Condition(){
 	if [ $pos -lt 0 ]
 	then
 		pos=0
 	fi
 	if [ $pos -eq 100 ]
 	then
-		game[$rollCount]=100
 		echo "Your position is 100"
-		echo "You have won!"
+		echo "Player $player won!"
 		exit
 	fi
 	if [ $pos -gt 100 ]
 	then
 		pos=$(($pos - $roll))
 	fi
-	game[$rollCount]=$pos
-	echo "Your position is: $pos"
-	rollCount=$(($rollCount + 1))
-done
+	echo Your position is:$pos
+}
+
+Game(){
+	while [ $pos -ne 100 ]
+	do
+		echo "*********Player:$player*********"
+		if [ $player -eq 1 ]
+		then
+			pos=$player1Pos
+			dice
+			Condition
+			player1Pos=$pos
+			player=2
+		else
+			pos=$player2Pos
+			dice
+			Condition
+			player2Pos=$pos
+			player=1
+		fi
+	done
+}
+Game
